eb85416f15adbac61e5c703760ee70eb49e0f85c
Benjamin Winkler <winkler@kerosieben.de>
2014-12-01 05:46:06 +0100

    fix order of files isn't static

--- tasks/asar.js
+++ tasks/asar.js
@@ -9,8 +9,39 @@
 'use strict';
 
 var path = require('path');
+var fs = require('fs');
 
-var asar = require('asar');
+var asar = require('asar');//!
+var Filesystem = require('asar/lib/filesystem');
+var disk = require('asar/lib/disk');
+
+var glob = require('glob');
+
+function generateAsarArchive(srcPath, destFile, cb) {
+  glob('**/*', {cwd: srcPath}, function(err, entries) {
+    if(err) { return cb(err); }
+    var filesystem = new Filesystem(srcPath);
+    var files = [];
+    var i, file, stat;
+
+    for(i in entries) {
+      file = path.join(process.cwd(), srcPath, entries[i]);
+      stat = fs.statSync(file);
+      if(stat.isDirectory()) {
+        filesystem.insertDirectoy(file);
+      }
+      else {
+        filesystem.insertFile(file, stat);
+        files.push(file);
+      }
+    }
+
+    console.log("files", files);
+
+    disk.writeFilesystem(destFile, filesystem, files);
+    cb(null);
+  });
+}
 
 module.exports = function(grunt) {
   grunt.registerMultiTask('asar', 'Generate atom-shell asar packages.', function() {
@@ -25,7 +56,7 @@ module.exports = function(grunt) {
     var filesLeft = this.files.length;
     var eachDone = function(f) {
       grunt.log.writeln('File ' + f.dest + ' created from ' + f.src[0]);
-      if(--filesLeft == 0) {
+      if(--filesLeft === 0) {
         done();
       }
     };
@@ -36,7 +67,8 @@ module.exports = function(grunt) {
       // to create the dir if necessary we're creating an empty file.
       grunt.file.write(dest, '');
       
-      asar.createPackage(f.src[0], dest, function(){eachDone(f);});
+      //asar.createPackage(f.src[0], dest, function(){eachDone(f);});
+      generateAsarArchive(f.src[0], dest, function(){eachDone(f);});
     });
   });
 };
